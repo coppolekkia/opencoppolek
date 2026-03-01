c015d8033202e96ca7304635c016b863965d9273
Josh Lehman <josh@martian.engineering>
2026-03-01 08:36:50 -0800

    fix(test): add registerContextEngine to diffs extension mock APIs

--- extensions/diffs/index.test.ts
+++ extensions/diffs/index.test.ts
@@ -30,6 +30,7 @@ describe("diffs plugin registration", () => {
       registerService() {},
       registerProvider() {},
       registerCommand() {},
+      registerContextEngine() {},
       resolvePath(input: string) {
         return input;
       },
@@ -98,6 +99,7 @@ describe("diffs plugin registration", () => {
       registerService() {},
       registerProvider() {},
       registerCommand() {},
+      registerContextEngine() {},
       resolvePath(input: string) {
         return input;
       },
