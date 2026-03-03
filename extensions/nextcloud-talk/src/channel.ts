c0e8d60aebbfd944ad037bbfb02dfb8b57a11038
kevin song <610833109@qq.com>
2026-03-03 17:18:38 +0800

    fix: correct abort-signal import in nextcloud-talk extension

--- extensions/nextcloud-talk/src/channel.ts
+++ extensions/nextcloud-talk/src/channel.ts
@@ -12,7 +12,7 @@ import {
   type OpenClawConfig,
   type ChannelSetupInput,
 } from "openclaw/plugin-sdk";
-import { waitForAbortSignal } from "../../../src/infra/abort-signal.js";
+import { waitForAbortSignal } from "openclaw/plugin-sdk";
 import {
   listNextcloudTalkAccountIds,
   resolveDefaultNextcloudTalkAccountId,
