openclaw/openclaw

fix(transcript): drop empty toolCallId toolResults during persistence and repair
Some providers occasionally emit assistant turns with stopReason=toolUse but no
valid tool call IDs. If an empty-id toolResult slips into session history, strict
providers can reject subsequent requests due to malformed tool-result pairing.

Changes:
- session-tool-result-guard: reject toolResult messages with empty/missing
  toolCallId before persistence
- session-transcript-repair: drop toolResult entries with empty/missing
  toolCallId during repair
- add regression tests for both guard and repair paths

Fixes #21985

src/agents/session-tool-result-guard.ts
@@ -184,16 +184,22 @@ export function installSessionToolResultGuard(
 
     if (nextRole === "toolResult") {
       const id = extractToolResultId(nextMessage as Extract<AgentMessage, { role: "toolResult" }>);
-      const toolName = id ? pending.get(id) : undefined;
-      if (id) {
-        pending.delete(id);
+
+      // Drop tool results with empty/missing toolCallId — they can't be paired
+      // with any assistant tool call and will corrupt the session for strict
+      // providers (Anthropic, MiniMax, Cloud Code Assist). See #21985.
+      if (!id) {
+        return undefined;
       }
+
+      const toolName = pending.get(id);
+      pending.delete(id);
       // Apply hard size cap before persistence to prevent oversized tool results
       // from consuming the entire context window on subsequent LLM calls.
       const capped = capToolResultSize(persistMessage(nextMessage));
       const persisted = applyBeforeWriteHook(
         persistToolResult(capped, {
-          toolCallId: id ?? undefined,
+          toolCallId: id,
           toolName,
           isSynthetic: false,
         }),

coppolekkia/opencoppolek

src/agents/session-tool-result-guard.ts
@@ -183,17 +183,23 @@ export function installSessionToolResultGuard(
 
     if (nextRole === "toolResult") {
       const id = extractToolResultId(nextMessage as Extract<AgentMessage, { role: "toolResult" }>);
-      const toolName = id ? pendingState.getToolName(id) : undefined;
-      if (id) {
-        pendingState.delete(id);
+
+      // Drop tool results with empty/missing toolCallId — they can't be paired
+      // with any assistant tool call and will corrupt the session for strict
+      // providers (Anthropic, MiniMax, Cloud Code Assist). See #21985.
+      if (!id) {
+        return undefined;
       }
+
+      const toolName = pendingState.getToolName(id);
+      pendingState.delete(id);
       const normalizedToolResult = normalizePersistedToolResultName(nextMessage, toolName);
       // Apply hard size cap before persistence to prevent oversized tool results
       // from consuming the entire context window on subsequent LLM calls.
       const capped = capToolResultSize(persistMessage(normalizedToolResult));
       const persisted = applyBeforeWriteHook(
         persistToolResult(capped, {
-          toolCallId: id ?? undefined,
+          toolCallId: id,
           toolName,
           isSynthetic: false,
         }),